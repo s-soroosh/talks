9e65fe8ad6724d0ee0bd1c25c4ef5f4fbccadfc5
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2021-01-10 18:09:15 +0100

    project skeletons

---
+++ aop-examples/spring-aop-examples/src/main/java/com/github/psychoir/talks/aop/Program.java
@@ -0,0 +1,13 @@
+package com.github.psychoir.talks.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Program {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Program.class,args);
+  }
+
+}
